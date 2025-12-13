908d290429814e9bbfb68b72d4950b4de7cd266f
Jeshki <karoliscibiras@gmail.com>
2025-12-13 18:59:50 +0200

    Fix lockfile and config

--- next.config.js
+++ next.config.js
@@ -1,27 +1,16 @@
-﻿// next.config.js
-
-// The import "./src/env.js" line is removed as it causes a SyntaxError in CJS mode.
-// The environment variables defined in src/env.js are typically loaded automatically 
-// by the Next.js/T3 boilerplate when the app starts.
+// next.config.js
+// Environment variables from src/env.js are loaded automatically by the app; no manual import here.
 
 /** @type {import("next").NextConfig} */
 const config = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "utfs.io", // Leid┼Šiame rodyti nuotraukas i┼Ī ─Źia
-                hostname: "utfs.io", // Leidžiame rodyti nuotraukas iš čia
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io", // Allow serving images from utfs.io
+      },
+    ],
+  },
 };
 
-// Naudojame CommonJS sintaks─Ö
-// Naudojame CommonJS sintaksę
 module.exports = config;
-
-module.exports = {
-  turbopack: { root: __dirname },
-  images: { remotePatterns: [{ protocol: "https", hostname: "utfs.io" }] },
-};
